a3cccc2ac2fbf520323f4e0d9e50ebdb1b8ae3cb
EC2 Default User <ec2-user@ip-172-31-46-47.ap-northeast-1.compute.internal>
2022-12-30 08:10:54 +0000

    [Add]app1.js,app.css編集

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,4 +1,11 @@
 /*global $*/
 $(function(){
-  $('.box1').hide();
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color': '#0000ff',
+      'height': '100px',
+      'width': '200px'
+    });
+    $('.box1').slideUp();
+  });
 });
